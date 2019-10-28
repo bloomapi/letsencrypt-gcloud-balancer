21d8f0971ac1af256b1b840dac3a9efae0c401f6
Trevor Porter <trkporter@ucdavis.edu>
2019-10-28 14:55:33 -0700

    Add CERT_ID_PREFIX and fix v02 url for monthly and init

--- init.sh
+++ init.sh
@@ -16,7 +16,7 @@ KEY=$(ls -1 /root/.lego/certificates | grep key\$)
 cat /root/.lego/certificates/$CERT /root/.lego/certificates/$CERT_ISSUER > cert.crt
 
 # Create name for new certificate in gcloud
-CERT_ID=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 16 | head -n 1)-cert
+CERT_ID=${CERT_ID_PREFIX}cert-$(cat /dev/urandom | tr -dc 'a-z' | fold -w 16 | head -n 1)
 OLD_CERT_ID=$(./google-cloud-sdk/bin/gcloud -q compute target-https-proxies list --filter "name=${TARGET_PROXY}" | sed -n 2p | awk '{print $2}')
 
 # Generate new gcloud certificate and attach to https proxy
